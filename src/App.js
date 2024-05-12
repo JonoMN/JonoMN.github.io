749caed811b761e0d309de050a87b469d2d65bf8
JonoMN <81200835+JonoMN@users.noreply.github.com>
2024-05-12 20:14:11 +1000

    Updated routing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Home from "./pages/home";
 import Blog from "./pages/blog";
+
+
 import Blog1_CORs from "./blogs/1_Cors";
+import Blog2_ReactHooks from "./blogs/2_ReactHooks";
+import Blog3_HTTPCacheControl from "./blogs/3_HTTPCacheControl";
+import Blog4_JavascriptHoisting from "./blogs/4_JavascriptHoisting";
+
+import Project2_FinanceTracker from "./projects/2_FinanceTracker";
+import Projects1_PeakPerformance from "./projects/1_PeakPerformance";
+
 import Projects from "./pages/projects";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
@@ -23,19 +32,22 @@ function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div>
         <header>
           <NavBar></NavBar>
         </header>
-        <body>
+        <div style={{ paddingTop: '90px' }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
+              <Route path="/projects/finance-tracker" element={<Project2_FinanceTracker />} />
+              <Route path="/projects/peak-performance" element={<Projects1_PeakPerformance />} />
             <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/CORs" element={<Blog1_CORs />} />
+              <Route path="/blog/CORs" element={<Blog1_CORs />} />
+              <Route path="/blog/React-Hooks" element={<Blog2_ReactHooks />} />
+              <Route path="/blog/http-cache-controls" element={<Blog3_HTTPCacheControl />} />
+              <Route path="/blog/javascript-hoisting" element={<Blog4_JavascriptHoisting />} />
           </Routes>
-        </body>
-      </div>
+        </div>
     </ThemeProvider>
   );
 }
