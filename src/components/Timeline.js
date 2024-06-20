d90b6ddd93a3167bb4ae2cb0aff11774fe3f9ca4
Jonathan Nelson <jonomnelson@gmail.com>
2024-06-20 16:34:55 +1000

    Updated timline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,25 +8,28 @@ const experiences = [
   {
     type: "Work Experience",
     items: [
-      { startDate: "Jun 2016", endDate: "Jun 2016", duration: "5 years", role: "Team Member", company: "Woolworths", description:"At Woolworths, I gained five years of experience in customer service, team collaboration, and time management, enhancing my communication skills and ability to work under pressure." },
-      { startDate: "May 2021", endDate: "Jun 2016", duration: "3 years", role: "Software & Systems Developer", company: "Mecha", description: "As a Software & Systems Developer at Mecha, I upgraded IT systems to MS365, including SharePoint for file management and Exchange for email. Collaborating with stakeholders, I integrated MS Power Apps, Forms, and Lists to automate work processes, significantly enhancing safety, efficiency, and overall organizational productivity." },
-      { startDate: "Oct 2022", endDate: "Jun 2016", duration: "3 years", role: "IT Officer", company: "ACRRM", description: "As an IT Officer at ACRRM, I collaborated with stakeholders on IT maintenance, repair, and troubleshooting. I created and maintained SharePoint sites, ensured security, developed Power Apps and forms, and managed user accounts. All tasks involved front-facing collaboration with internal and external stakeholders, ensuring efficient IT operations and enhancing overall productivity." },
-      { startDate: "Feb 2016", endDate: "Jun 2016", duration: "Currently", role: "Graduate Fullstack Developer", company: "ACRRM", description: "As a Graduate Fullstack Developer at ACRRM, I develop and maintain applications using React and .NET. I manage version control with Git, utilize CI/CD pipelines for seamless deployment, and adhere to agile (Scrum) methodologies. My role involves collaborating with cross-functional teams, ensuring efficient software delivery, and enhancing system integration." }
+      { startDate: "Jun 2016", endDate: "May 2021", duration: "5 years", role: "Team Member", company: "Woolworths", description:"At Woolworths, I gained five years of experience in customer service, team collaboration, and time management, enhancing my communication skills and ability to work under pressure." },
+      { startDate: "May 2021", endDate: "Sep 2022", duration: "1 year, 5 months", role: "Software & Systems Developer", company: "Mecha", description: "As a Software & Systems Developer at Mecha, I upgraded IT systems to MS365, including SharePoint for file management and Exchange for email. Collaborating with stakeholders, I integrated MS Power Apps, Forms, and Lists to automate work processes, significantly enhancing safety, efficiency, and overall organizational productivity." },
+      { startDate: "Oct 2022", endDate: "Feb 2024", duration: "1 year, 5 months", role: "IT Officer", company: "ACRRM", description: "As an IT Officer at ACRRM, I collaborated with stakeholders on IT maintenance, repair, and troubleshooting. I created and maintained SharePoint sites, ensured security, developed Power Apps and forms, and managed user accounts. All tasks involved front-facing collaboration with internal and external stakeholders, ensuring efficient IT operations and enhancing overall productivity." },
+      { startDate: "Feb 2024", endDate: "Present", duration: "Currently", role: "Graduate Fullstack Developer", company: "ACRRM", description: "As a Graduate Fullstack Developer at ACRRM, I develop and maintain applications using React and .NET. I manage version control with Git, utilize CI/CD pipelines for seamless deployment, and adhere to agile (Scrum) methodologies. My role involves collaborating with cross-functional teams, ensuring efficient software delivery, and enhancing system integration." }
     ],
   },
   {
     type: "Volunteer Experience",
     items: [
-      { startDate: "Sep 2021", endDate: "Jun 2016", duration: "1 year", role: "Corporate Events Director", company: "QUT BAMSA", description: "The corporate events director is a managerial role that takes charge of designing, planning, and communicating with venues for all corporate events throughout the year." },
-      { startDate: "Sep 2022", endDate: "Jun 2016", duration: "1 year", role: "President", company: "QUT BAMSA", description: "The president represents BAMSA as the spokesperson, sets the strategic direction of the club, prioritises goals and oversees the delivery of outcomes, presides and leads during club meetings and preserves order, acting as the club's leader and ambassador to represent team members." },
-      { startDate: "Jan 2024", endDate: "Jun 2016", duration: "Currently", role: "Emerging Professional Committee Member", company: "Australian Computer Society", description: "The ACS Queensland Emerging Professionals Committee supports and encourages students and recent graduates interested in technology careers to engage with the local ACS Queensland ICT Community. It aims to help the ACS Queensland Branch by acquiring new members, increasing member retention, strategising for student-focused events, and fostering collaboration with industry and education providers." },
+      { startDate: "Sep 2021", endDate: "Sep 2022", duration: "1 year", role: "Corporate Events Director", company: "QUT BAMSA", description: "The corporate events director is a managerial role that takes charge of designing, planning, and communicating with venues for all corporate events throughout the year." },
+      { startDate: "Sep 2022", endDate: "Sep 2023", duration: "1 year", role: "President", company: "QUT BAMSA", description: "The president represents BAMSA as the spokesperson, sets the strategic direction of the club, prioritises goals and oversees the delivery of outcomes, presides and leads during club meetings and preserves order, acting as the club's leader and ambassador to represent team members." },
+      { startDate: "Jan 2024", endDate: "Present", duration: "Currently", role: "Emerging Professional Committee Member", company: "Australian Computer Society", description: "The ACS Queensland Emerging Professionals Committee supports and encourages students and recent graduates interested in technology careers to engage with the local ACS Queensland ICT Community. It aims to help the ACS Queensland Branch by acquiring new members, increasing member retention, strategising for student-focused events, and fostering collaboration with industry and education providers." },
+      { startDate: "May 2024", endDate: "Present", duration: "Currently", role: "Young Tech Ambassador", company: "Australian Computer Society", description: "Currently volunteering as a Young Tech Ambassador with the ACS YTA Program, funded by Queensland's Digital Professional Workforce Action Plan. I'm helping high school students get excited about tech careers while boosting my own leadership and communication skills." },
     ],
   },
 ];
 
 function TimelineComponent() {
   const [collapseStates, setCollapseStates] = useState(
-    experiences.map(exp => exp.items.map(() => false))
+    experiences.map(exp => 
+      exp.items.map((_, index) => index === exp.items.length - 1)
+    )
   );
 
   const handleToggle = (expIndex, itemIndex) => {
